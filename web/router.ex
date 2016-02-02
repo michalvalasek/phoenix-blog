dae9c6a011421d682425b83c8dc63ad3d97b8c84
Michal Valasek <michal.valasek@gmail.com>
2016-02-02 23:18:30 +0100

    Allow users to login/logout.

--- web/router.ex
+++ web/router.ex
@@ -20,6 +20,7 @@ defmodule Pxblog.Router do
 
     resources "/posts", PostController
     resources "/users", UserController
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
 
   # Other scopes may use custom stacks.
