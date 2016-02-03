39ff8348df076e9d2cf062eddf1bdb1b2865c451
Michal Valasek <michal.valasek@gmail.com>
2016-02-03 01:16:20 +0100

    Only allow User to modify his own Posts.

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -5,6 +5,7 @@ defmodule Pxblog.PostController do
 
   plug :scrub_params, "post" when action in [:create, :update]
   plug :assign_user
+  plug :authorize_user when action in [:new, :create, :edit, :update, :delete]
 
   def index(conn, _params) do
     posts = Repo.all(assoc(conn.assigns[:user], :posts))
@@ -95,6 +96,14 @@ defmodule Pxblog.PostController do
 
   defp authorize_user(conn, _opts) do
     user = get_session(conn, :current_user)
-    if user && Integer.to_string(user.id) == conn.params["user_id"]
+    if user && Integer.to_string(user.id) == conn.params["user_id"] do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You are not authorized to modify this post!")
+      |> redirect(to: page_path(conn, :index))
+      |> halt
+    end
+
   end
 end
