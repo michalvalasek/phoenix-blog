b7014d5edcfc87ed14d1ca966cfaa421265140c8
Michal Valasek <michal.valasek@gmail.com>
2016-02-09 18:11:50 +0100

    Bump exrm version.

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Pxblog.Mixfile do
      {:comeonin, "~> 2.1"},
      {:ex_machina, "~> 0.6", only: :test},
      {:earmark, "~> 0.2"},
-     {:exrm, "~> 0.19"}]
+     {:exrm, "~> 1.0.0-rc7"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
